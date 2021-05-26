0a8422ed49f3b837bcefb1077c1822a68cf2abbb
Wajeeha19 <bsef18m009@pucit.edu.pk>
2021-05-26 15:08:04 +0500

    Fragments

---
+++ Fragments/app/src/main/java/org/haqnawaz/fragments/MainActivity.java
@@ -0,0 +1,27 @@
+package org.haqnawaz.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentTransaction fragmentTransaction;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Fragment01(View view) {
+
+    }
+
+    public void Fragment02(View view) {
+    }
+
+}
